d7c8c83792cd2db634e7b9376568d4c8c3ce79e2
Oliver Gugger <gugger@gmail.com>
2020-10-26 17:55:28 +0100

    lightning: add ListPermissions

--- lightning_client.go
+++ lightning_client.go
@@ -163,6 +163,11 @@ type LightningClient interface {
 	// of newly added/settled invoices.
 	SubscribeInvoices(ctx context.Context, req InvoiceSubscriptionRequest) (
 		<-chan *Invoice, <-chan error, error)
+
+	// ListPermissions returns a list of all RPC method URIs and the
+	// macaroon permissions that are required to access them.
+	ListPermissions(ctx context.Context) (map[string][]MacaroonPermission,
+		error)
 }
 
 // Info contains info about the connected lnd node.
@@ -3041,4 +3046,50 @@ func (s *lightningClient) SubscribeInvoices(ctx context.Context,
 	}()
 
 	return invoiceUpdates, streamErr, nil
+}
+
+// MacaroonPermission is a struct that holds a permission entry, consisting of
+// an entity and an action.
+type MacaroonPermission struct {
+	// Entity is the entity a permission grants access to.
+	Entity string
+
+	// Action is the action that is granted by a permission.
+	Action string
+}
+
+// String returns the human readable representation of a permission.
+func (p *MacaroonPermission) String() string {
+	return fmt.Sprintf("%s:%s", p.Entity, p.Action)
+}
+
+// ListPermissions returns a list of all RPC method URIs and the macaroon
+// permissions that are required to access them.
+func (s *lightningClient) ListPermissions(
+	ctx context.Context) (map[string][]MacaroonPermission, error) {
+
+	rpcCtx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+
+	rpcCtx = s.adminMac.WithMacaroonAuth(rpcCtx)
+	perms, err := s.client.ListPermissions(
+		rpcCtx, &lnrpc.ListPermissionsRequest{},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string][]MacaroonPermission)
+	for methodURI, list := range perms.MethodPermissions {
+		permissions := list.Permissions
+		result[methodURI] = make([]MacaroonPermission, len(permissions))
+		for idx, entry := range permissions {
+			result[methodURI][idx] = MacaroonPermission{
+				Entity: entry.Entity,
+				Action: entry.Action,
+			}
+		}
+	}
+
+	return result, nil
 }
