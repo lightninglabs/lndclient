f99c6caa86d322fff66c22679cd6554b922addc2
positiveblue <jordi.montes@qualitechlabs.com>
2022-03-28 16:28:36 +0200

    mod+signer: upgrade lnd to version with taproot support

--- signer_client.go
+++ signer_client.go
@@ -94,6 +94,11 @@ type SignDescriptor struct {
 	// script (PkScript).
 	WitnessScript []byte
 
+	// TaprootKeySpend indicates that instead of a witness script being
+	// spent by the signature that results from this signing request, a
+	// taproot key spend is performed instead.
+	TaprootKeySpend bool
+
 	// Output is the target output which should be signed. The PkScript and
 	// Value fields within the output should be properly populated,
 	// otherwise an invalid signature may be generated.
@@ -150,7 +155,8 @@ func marshallSignDescriptors(signDescriptors []*SignDescriptor,
 		}
 
 		rpcSignDescs[i] = &signrpc.SignDescriptor{
-			WitnessScript: signDesc.WitnessScript,
+			WitnessScript:   signDesc.WitnessScript,
+			TaprootKeySpend: signDesc.TaprootKeySpend,
 			Output: &signrpc.TxOut{
 				PkScript: signDesc.Output.PkScript,
 				Value:    signDesc.Output.Value,
