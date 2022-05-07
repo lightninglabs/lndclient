c59d42f392123d75b02804b598e191ba4e6ea308
Olaoluwa Osuntokun <laolu32@gmail.com>
2022-05-06 17:46:03 -0700

    macaroons: update to new macaroons pkg version
    In this commit we update to the new version of the macaroons package
    that doesn't require the caller to use the default implementation for
    the root key store. Instead, the caller can pass in an implementation,
    and based on that we'll change our behavior.

--- macaroon_service_test.go
+++ macaroon_service_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/lightningnetwork/lnd/kvdb"
 	"github.com/lightningnetwork/lnd/macaroons"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/macaroon-bakery.v2/bakery"
 )
 
 // TestMacaroonServiceMigration tests that a client that was using a macaroon
@@ -24,14 +25,21 @@ func TestMacaroonServiceMigration(t *testing.T) {
 	require.NoError(t, err)
 	defer os.RemoveAll(tempDirPath)
 
+	db, err := kvdb.GetBoltBackend(&kvdb.BoltBackendConfig{
+		DBPath:     tempDirPath,
+		DBFileName: "macaroons.db",
+		DBTimeout:  defaultDBTimeout,
+	})
+	require.NoError(t, err)
+	rks, err := macaroons.NewRootKeyStorage(db)
+	require.NoError(t, err)
+
 	// The initial config we will use has an empty DB password.
 	cfg := &MacaroonServiceConfig{
-		DBPath:           tempDirPath,
-		DBFileName:       "macaroons.db",
-		DBTimeout:        defaultDBTimeout,
 		MacaroonLocation: "testLocation",
 		MacaroonPath:     tempDirPath,
 		DBPassword:       []byte{},
+		RootKeyStore:     rks,
 	}
 
 	// Create a new macaroon service with an empty password.
@@ -90,25 +98,16 @@ func TestMacaroonServiceMigration(t *testing.T) {
 
 type testMacaroonService struct {
 	*macaroons.Service
-	db kvdb.Backend
+	rks bakery.RootKeyStore
 }
 
 func createTestService(cfg *MacaroonServiceConfig) (*testMacaroonService,
 	error) {
 
-	db, err := kvdb.GetBoltBackend(&kvdb.BoltBackendConfig{
-		DBPath:     cfg.DBPath,
-		DBFileName: cfg.DBFileName,
-		DBTimeout:  cfg.DBTimeout,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("unable to load macaroon db: "+
-			"%v", err)
-	}
-
 	// Create the macaroon authentication/authorization service.
 	service, err := macaroons.NewService(
-		db, cfg.MacaroonLocation, cfg.StatelessInit, cfg.Checkers...,
+		cfg.RootKeyStore, cfg.MacaroonLocation, cfg.StatelessInit,
+		cfg.Checkers...,
 	)
 	if err != nil {
 		return nil, fmt.Errorf("unable to set up macaroon "+
@@ -117,14 +116,16 @@ func createTestService(cfg *MacaroonServiceConfig) (*testMacaroonService,
 
 	return &testMacaroonService{
 		Service: service,
-		db:      db,
+		rks:     cfg.RootKeyStore,
 	}, nil
 }
 
 func (s *testMacaroonService) stop() error {
 	var returnErr error
-	if err := s.db.Close(); err != nil {
-		returnErr = err
+	if eRKS, ok := s.rks.(macaroons.ExtendedRootKeyStore); ok {
+		if err := eRKS.Close(); err != nil {
+			returnErr = err
+		}
 	}
 
 	if err := s.Close(); err != nil {
